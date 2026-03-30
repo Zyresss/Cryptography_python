72589848fabe38da8442797f5c0a0bb9b8ee23e4
Fahd Djedi <djedifahd0@gmail.com>
2026-03-31 00:07:20 +0200

    Implement affine cipher class with encryption method

---
+++ classical_cryptography/affine.py
@@ -0,0 +1,41 @@
+from alphabet.alphabet import AlphabetMap, ReverseAlphabetMap
+
+class affine :
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def encrypt(self, plaintext):
+        ciphertext = ""
+        plaintext = plaintext.replace(" ", "")
+
+
+# class affine :
+#     def __init__(self, a, b):
+#         self.a = a
+#         self.b = b
+
+#     def encrypt(self, plaintext):
+#         ciphertext = ""
+#         plaintext = plaintext.replace(" ", "")
+#         for char in plaintext.upper():
+#             if char.isalpha():
+#                 x = AlphabetMap[char]
+#                 y = (self.a * x + self.b) % 26
+#                 ciphertext += ReverseAlphabetMap[y]
+#             else:
+#                 ciphertext += char
+#         return ciphertext
+    
+#     def decrypt(self, ciphertext):
+#         plaintext = ""
+#         ciphertext = ciphertext.replace(" ", "")
+#         for char in ciphertext.upper():
+#             if char.isalpha():
+#                 y = AlphabetMap[char]
+#                 a_inv = pow(self.a, -1, 26)
+#                 x = (a_inv * (y - self.b)) % 26
+#                 plaintext += ReverseAlphabetMap[x]
+#             else:
+#                 plaintext += char
+#         return plaintext
